1a65cc02383d58ab26e73c44e982adbafac499ea
chaimaeMazzouz <mazzouz.chaimae.dev@gmail.com>
2020-10-30 23:55:29 +0100

    ADO.NET - CRUD Operations

---
+++ ConnectedArchitectureIntro/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace ConnectedArchitectureIntro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textID = new System.Windows.Forms.TextBox();
+            this.textFirstName = new System.Windows.Forms.TextBox();
+            this.textLastName = new System.Windows.Forms.TextBox();
+            this.textEmail = new System.Windows.Forms.TextBox();
+            this.textPhone = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.addBtn = new System.Windows.Forms.Button();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.UpdateBtn = new System.Windows.Forms.Button();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textID
+            // 
+            this.textID.Location = new System.Drawing.Point(124, 37);
+            this.textID.Name = "textID";
+            this.textID.Size = new System.Drawing.Size(100, 20);
+            this.textID.TabIndex = 0;
+            // 
+            // textFirstName
+            // 
+            this.textFirstName.Location = new System.Drawing.Point(124, 74);
+            this.textFirstName.Name = "textFirstName";
+            this.textFirstName.Size = new System.Drawing.Size(100, 20);
+            this.textFirstName.TabIndex = 1;
+            // 
+            // textLastName
+            // 
+            this.textLastName.Location = new System.Drawing.Point(124, 117);
+            this.textLastName.Name = "textLastName";
+            this.textLastName.Size = new System.Drawing.Size(100, 20);
+            this.textLastName.TabIndex = 2;
+            // 
+            // textEmail
+            // 
+            this.textEmail.Location = new System.Drawing.Point(124, 159);
+            this.textEmail.Name = "textEmail";
+            this.textEmail.Size = new System.Drawing.Size(100, 20);
+            this.textEmail.TabIndex = 3;
+            // 
+            // textPhone
+            // 
+            this.textPhone.Location = new System.Drawing.Point(124, 206);
+            this.textPhone.Name = "textPhone";
+            this.textPhone.Size = new System.Drawing.Size(100, 20);
+            this.textPhone.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "lD : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "First Name :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Last Name : ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(36, 162);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Email : ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(36, 213);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Phone :";
+            // 
+            // addBtn
+            // 
+            this.addBtn.Location = new System.Drawing.Point(289, 33);
+            this.addBtn.Name = "addBtn";
+            this.addBtn.Size = new System.Drawing.Size(75, 23);
+            this.addBtn.TabIndex = 10;
+            this.addBtn.Text = "Add";
+            this.addBtn.UseVisualStyleBackColor = true;
+            this.addBtn.Click += new System.EventHandler(this.addBtn_Click);
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(289, 71);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(75, 23);
+            this.saveBtn.TabIndex = 11;
+            this.saveBtn.Text = "Save";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // searchBtn
+            // 
+            this.searchBtn.Location = new System.Drawing.Point(289, 114);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(75, 23);
+            this.searchBtn.TabIndex = 12;
+            this.searchBtn.Text = "Search";
+            this.searchBtn.UseVisualStyleBackColor = true;
+            this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
+            // 
+            // UpdateBtn
+            // 
+            this.UpdateBtn.Location = new System.Drawing.Point(290, 156);
+            this.UpdateBtn.Name = "UpdateBtn";
+            this.UpdateBtn.Size = new System.Drawing.Size(75, 23);
+            this.UpdateBtn.TabIndex = 13;
+            this.UpdateBtn.Text = "Update";
+            this.UpdateBtn.UseVisualStyleBackColor = true;
+            this.UpdateBtn.Click += new System.EventHandler(this.UpdateBtn_Click);
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Location = new System.Drawing.Point(290, 203);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(75, 23);
+            this.deleteBtn.TabIndex = 14;
+            this.deleteBtn.Text = "Delete";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 245);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.UpdateBtn);
+            this.Controls.Add(this.searchBtn);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.addBtn);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textPhone);
+            this.Controls.Add(this.textEmail);
+            this.Controls.Add(this.textLastName);
+            this.Controls.Add(this.textFirstName);
+            this.Controls.Add(this.textID);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textID;
+        private System.Windows.Forms.TextBox textFirstName;
+        private System.Windows.Forms.TextBox textLastName;
+        private System.Windows.Forms.TextBox textEmail;
+        private System.Windows.Forms.TextBox textPhone;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button addBtn;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.Button searchBtn;
+        private System.Windows.Forms.Button UpdateBtn;
+        private System.Windows.Forms.Button deleteBtn;
+    }
+}
+
---
+++ ConnectedArchitectureIntro/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ConnectedArchitectureIntro
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        void Reset()
+        {
+            textID.Text = textFirstName.Text = textLastName.Text = textEmail.Text = textPhone.Text = "";
+            textID.Focus();
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            con = new SqlConnection(ConfigurationManager.ConnectionStrings["conStr"].ConnectionString);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void addBtn_Click(object sender, EventArgs e)
+        {
+            Reset();
+            string command = "select max(Id) + 1 from customer";
+            cmd = new SqlCommand(command, con);
+            con.Open();
+            string id = cmd.ExecuteScalar().ToString();
+            con.Close();
+            textID.Text = id;
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            string id = textID.Text;
+            string firstName = textFirstName.Text;
+            string lastName = textLastName.Text;
+            string email = textEmail.Text;
+            string phone = textPhone.Text;
+            string command = string.Format("insert into customer values({0} ,'{1}','{2}','{3}',{4})",id,firstName,lastName,email,phone);
+            cmd = new SqlCommand(command, con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            Reset();
+        }
+
+        private void searchBtn_Click(object sender, EventArgs e)
+        {
+            string id = textID.Text;
+            string command = "select * from customer where Id ="+id;
+            cmd = new SqlCommand(command, con);
+            con.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.Read())
+            {
+                textFirstName.Text = dr[1].ToString();
+                textLastName.Text = dr[2].ToString();
+                textEmail.Text = dr[3].ToString();
+                textPhone.Text = dr[4].ToString();
+            }
+            else
+            {
+                MessageBox.Show("cannot find the recored..");
+                Reset();
+            }
+            dr.Close();
+            con.Close();
+        }
+
+        private void UpdateBtn_Click(object sender, EventArgs e)
+        {
+            string id = textID.Text;
+            string firstName = textFirstName.Text;
+            string lastName = textLastName.Text;
+            string email = textEmail.Text;
+            string phone = textPhone.Text;
+            string command = string.Format("update customer set Id={0} , FirstName='{1}', LastName= '{2}', EmailAdress='{3}',PhoneNumber= {4}",id,firstName,lastName,email,phone);
+            cmd = new SqlCommand(command, con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Recored Update..");
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            string id = textID.Text;
+            string command = "delete from customer where Id="+id;
+            cmd = new SqlCommand(command, con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Recored Delete..");
+        }
+    }
+}
